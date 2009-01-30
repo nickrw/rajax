60fbf4702c9d574bd365adf8cf1d8e1c19aa08ae
Nick <nick@njrw.co.uk>
2009-01-30 15:18:35 +0000

    Added ajaxform link to main examples page

--- controller/main.rb
+++ controller/main.rb
@@ -14,6 +14,11 @@ class MainController < Controller
       <p>The <strong>click2delete_button</strong> method gives you the HTML for a button which, when clicked, deletes an iitem from the page if a success response is given by the ajax controller. Parameters you specify are passed to the ajax controller.</p>
       <p>The <strong>click2delete_wrapper</strong> method wraps HTML around the item you want to be deletable. This method must contain the output of a click2delete_button for it to be of any use. You specify to this method what sort of tag you would like the wrapper to use, E.G. :tr for a table-row or :div for a block element.</p>
       <p><strong><a href='/click2delete/'>View examples</a></strong></p>
+      
+      <hr />
+      <h3>Handle forms submission/response in-page</h3>
+      <p>The <strong>ajaxform</strong> method wraps <form> tags around the given input which stop the form from submitting normally, instead sending the data to the ajax controller.</p>
+      <p><strong><a href='/ajaxform/'>View example</a></strong></p>
     }
   end
 end
